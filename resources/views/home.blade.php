fbf1a33a4c8c31a4b1e61f332fbad6d81a66d428
Vale2197 <valentinatrebbi97@gmail.com>
2022-01-11 16:29:54 +0100

    stampate card film a schermo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,37 @@
 
 @section('content')
 
-    <h1>CIAO</h1>
+    <div class="container">
+
+        <div class="row g-3 py-5 justify-content-center">
+
+            <h1 class="mb-3">
+                MOVIES:
+            </h1>
+
+            @forelse ($movies as $movie)
+    
+                <div class="card mx-2" style="width: 18rem;">
+                    <div class="card-body">
+                    <h5 class="card-title">{{ $movie->title }}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">original title: {{$movie->original_title}}</h6>
+                    <p class="card-text"> nationality: {{ $movie->nationality }}</p>
+                    <span class="card-link"> {{ $movie->date }} </span>
+                    <span class="card-link"> {{ $movie->vote }} </span>
+                    </div>
+                </div>
+                
+            @empty
+                <p>
+                    non ci sono film al momento..    
+                </p>    
+    
+            @endforelse
+
+        </div>
+
+
+
+    </div>
 
 @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,11 +4,23 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
         <title>Laravel @yield('title') </title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <style>
+            body {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            .card {
+                border-radius: 1rem;
+                box-shadow: 2px 2px 2px rgb(202, 202, 202);
+            }
+        </style>
 
     </head>
     <body>
